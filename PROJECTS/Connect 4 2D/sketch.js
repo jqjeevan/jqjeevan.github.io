3f7c0a66618a53e8067430ce881ce86b8f63e672
jqjeevan <jq.jeevan@gmail.com>
2021-02-08 10:51:47 -0600

    changed name, with comments

--- PROJECTS/Connect 4 2D/sketch.js
+++ PROJECTS/Connect 4 2D/sketch.js
@@ -1,51 +1,105 @@
+// Jeevan Prakash
+// February 8, 2021
+// Computer Science 30
+// Grid/2D Array Assignment - Connect 4 (Without Gravity)
+// Mr. Schellenberg
+
+// Constants used for the array and used to determine the height and width of circles in array
 const ROWS = 7;
 const COLS = 8;
+
+// Variables used in the program
 let grid, cellWidth, cellHeight;
 let player = false;
 let playertoggle = "blue";
 let click;
 let bgmusic;
+let endImage;
 let ifWin = 0;
+let whichScreen = 1;
 
+// Preloads 2 sounds and an image for the end screen
 function preload(){
   click = loadSound("assets/click.wav");
   bgmusic = loadSound("assets/backgroundmusic.mp3");
+  endImage = loadImage("assets/winscreen.gif");
 }
 
 function setup() {
+  // Starts playing the background sound
   bgmusic.loop();
 
+  // Loads canvas in the middle of screen
   let myCanvas = createCanvas(windowWidth*0.7, windowHeight*0.8);
   myCanvas.position(windowWidth*0.15, windowHeight*0.1);
 
+  // Defines the grid, and determines the cell(circle) width
   grid = createEmptyGrid(COLS, ROWS);
   cellWidth = width / COLS;
   cellHeight = height / ROWS;
-
-  background(102, 0, 102, 0);
 }
 
 function draw() {
+  // If whichScreen is set as 1, it calls the playScreen function which plays the game
+  if (whichScreen === 1){
+    playScreen();
 
+  }
+  // After the game is won, whichScreen is set at 2 which ends the game
+  else if (whichScreen === 2){
+    endScreen();
+  }
+}
+
+// Sets the background for the game screen, displays the grid, and starts the wincheck function
+function playScreen() {
+  background(102, 0, 102, 0);
   displayGrid();
   winCheck();
+}
 
+function endScreen() {
+  // Changes background to the gif image
+  background(endImage);
+
+  // If the ifWin Variable is changed to a 1, then it will check who won
+  if (ifWin === 1) {
+    // If player toggle (which changes after every click) is red, say red wins
+    if (playertoggle === "red"){
+      fill(255, 0, 0);
+      textAlign(CENTER);
+      textSize(50);
+      textFont("georgia");
+      text("RED WINS! Press Ctrl+r to restart", width / 2, height / 2);
+    }
+    // If its not red it will be blue, so it will display blue wins
+    else {
+      fill(0, 0, 255);
+      textAlign(CENTER);
+      textSize(50);
+      textFont("georgia");
+      text("BLUE WINS! Press Ctrl+r to restart", width / 2, height / 2);
+    }
+  }
 }
 
 function mousePressed() {
+  // Used to define the x and y vlaue which is used to determine which color the cell(circle) will be
   let x = Math.floor(mouseX / cellWidth);
   let y = Math.floor(mouseY / cellHeight);
 
+  // Fills the cell with the color determined by the array value
   changePlayer(x, y);
 
+  // When mouse pressed a sound is made
   click.play();
 
+  // Switches the player color string to be used to say who wins in endScreen
   finalWinToggle();
-
 }
 
-
 function displayGrid() {
+  // Draws tha grid circles, and according to what the grid x and y are assigned, fills the color
   for (let y=0; y<ROWS; y++) {
     for (let x=0; x<COLS; x++) {
       if (grid[y][x] === 0) {
@@ -62,6 +116,7 @@ function displayGrid() {
   }
 }
 
+// Creates an empty grid used for the displaygrid
 function createEmptyGrid(cols, rows) {
   let empty = [];
   for (let y=0; y<rows; y++) {
@@ -73,6 +128,7 @@ function createEmptyGrid(cols, rows) {
   return empty;
 }
 
+// Used to determine the grid value which then changes the color
 function changePlayer(x, y){
  
   if (grid[y][x] === 0) {
@@ -87,8 +143,9 @@ function changePlayer(x, y){
   }
 }
 
+// Checks the array for a connect 4, if it does the ifWIn value is changed to 1, which starts the end screen
 function winCheck() {
-  // Test Horizontal 4
+  // Checks Horizonatal
   for (let y = 0; y < ROWS; y++) {
     for (let x = 0; x <= COLS-4; x++) {
       const test = grid[y][x];
@@ -106,7 +163,7 @@ function winCheck() {
     }
   }
   
-  // Test Vertical 4
+  // Checks Vertical
   for (let y = 0; y <= ROWS-4; y++) {
     for (let x = 0; x < COLS; x++) {
       const test = grid[y][x];
@@ -124,7 +181,7 @@ function winCheck() {
     }
   }
   
-  // Test Diagonal 4
+  // Checks Diagonal
   for (let y = 0; y <= ROWS-4; y++) {
     for (let x = 0; x <= COLS-4; x++) {
       const test = grid[y][x];
@@ -142,7 +199,7 @@ function winCheck() {
     }
   }
   
-  // Test Antidiagonal 4
+  // Checks Antidiagonal
   for (let y = 0; y <= ROWS-4; y++) {
     for (let x = 4; x < COLS; x++) {
       const test = grid[y][x];
@@ -160,24 +217,11 @@ function winCheck() {
     }
   }
   if (ifWin === 1) {
-    if (playertoggle === "red"){
-      fill(255, 0, 0);
-      textAlign(CENTER);
-      textSize(50);
-      textFont("georgia");
-      text("Red Wins!", width / 2, height / 3);
-    }
-    else {
-      fill(0, 0, 255);
-      textAlign(CENTER);
-      textSize(50);
-      textFont("georgia");
-      text("Blue Wins!", width / 2, height / 3);
-    }
+    whichScreen = 2;
   }
-  console.log(ifWin);
 }
 
+// Toggles red or blue for final win screen
 function finalWinToggle(){
   if (playertoggle === "blue"){
     playertoggle = "red";
@@ -185,8 +229,4 @@ function finalWinToggle(){
   else {
     playertoggle = "blue";
   }
-}
-
-function openScreen() {
-  
 }
