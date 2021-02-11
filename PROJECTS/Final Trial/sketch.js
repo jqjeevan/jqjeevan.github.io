dc7113932bf4e226bc4d2535103c0b6f9834c1b8
jqjeevan <jqjeevan@users.noreply.github.com>
2021-02-11 12:25:50 -0600

    indent

--- PROJECTS/Final Trial/sketch.js
+++ PROJECTS/Final Trial/sketch.js
@@ -4,13 +4,13 @@ let bird;
 let world, engine;
 
 function setup() {
-createCanvas(600, 400);
-engine = Matter.Engine.create();
-world = engine.world;
-ground = new Ground(width/2, height-20, width, 20);
-box = new Box (450, 300, 50, 75);
-bird = new Bird(50, 300, 25);
-  }
+  createCanvas(600, 400);
+  engine = Matter.Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2, height-20, width, 20);
+  box = new Box (450, 300, 50, 75);
+  bird = new Bird(50, 300, 25);
+}
 
 function draw() {
   background(0);
